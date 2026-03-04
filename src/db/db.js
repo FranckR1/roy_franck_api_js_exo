01ee7b0ba96aff84c3e4885df55743c92bf21e70
Franck <franck.rd1@outlook.fr>
2026-03-04 14:17:44 +0100

    feat: add package dotenv for my variable and add package mariadb

---
+++ src/db/db.js
@@ -0,0 +1,21 @@
+const mariadb = require('mariadb');
+
+const pool = mariadb.createPool({
+    host: process.env.MARIADB_HOST,
+    user: process.env.MARIADB_USER,
+    password: process.env.MARIADB_ROOT_PASSWORD,
+    database: process.env.MARIADB_DATABASE,
+});
+
+pool.getConnection()
+    .then(conn => {
+        console.log('Connexion à MariaDB réussie !');
+        conn.release();
+    })
+    .catch(err => {
+        console.error('Erreur de connexion à MariaDB:', err.message);
+    });
+
+export const getPool = () => {
+    return pool;
+};
