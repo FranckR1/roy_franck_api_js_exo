0dd3012b73f9f52d701410d3cd8ddfd8928dad7c
Franck <franck.rd1@outlook.fr>
2026-03-04 12:19:15 +0100

    feat: create file docker compose

--- src/index.js
+++ src/index.js
@@ -9,6 +9,5 @@ const productsRoute = require('../src/modules/products/product.route');
 app.use('/api', productsRoute);
 
 app.listen(PORT, () => {
-    console.log(`🚀
- Serveur démarré sur http://localhost:${PORT}`);
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
