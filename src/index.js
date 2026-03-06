7ca03b437c6c867075013702c08dddd9f34b025d
Franck <franck.rd1@outlook.fr>
2026-03-06 15:51:03 +0100

    feat : create swagger doc for my api

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ const PORT = 3000;
 require('dotenv').config();
 const { pool } = require('../src/db/db');
 const { logger } = require('../src/core/logger/logger');
+const swaggerUi = require('swagger-ui-express');
+const { swaggerSpec } = require('../src/config/swagger');
 
 app.use(express.json());
 
 const productsRoute = require('../src/modules/products/product.route');
 
 app.use('/api', productsRoute);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.listen(PORT, () => {
     logger.info(`Serveur démarré sur http://localhost:${PORT}`);
--- src/modules/products/product.route.js
+++ src/modules/products/product.route.js
@@ -4,8 +4,61 @@ const productsController = require('./product.controller');
 const { validateData } = require("../../core/middlewares/middleware.zod");
 const { productSchema } = require("./product.schema");
 
+/**
+ * @swagger
+ * tags:
+ *  name: Products
+ *  description: routes de base des products
+ *
+ */
+
 router.get('/products', productsController.getAllProducts);
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   get:
+ *     summary: Récupère un produit par ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Produit trouvé
+ *       404:
+ *         description: Produit introuvable
+ */
+
 router.get('/products/:id', productsController.getProductById);
+
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     summary: Crée un produit
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *               category:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Produit créé
+ *       400:
+ *         description: Données invalides
+ */
+
 router.post('/products', validateData(productSchema), productsController.createProduct);
 router.put('/products/:id', productsController.updateProduct);
 router.delete('/products/:id', productsController.deleteProduct);
